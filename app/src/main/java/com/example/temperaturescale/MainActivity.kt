1f706323452d62a179095f64fd2424fcc26644d3
Nirbhay Malhotra <nirbhaymalhotra2@gmail.com>
2023-10-02 23:36:47 -0400

    Temperature Activity

---
+++ app/src/main/java/com/example/temperaturescale/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.example.temperaturescale
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var celsiusTextView:TextView;
+    private lateinit var fahrenheitTextView:TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        celsiusTextView = findViewById(R.id.celsiusTextView)
+
+        fahrenheitTextView = findViewById(R.id.FahrenheitTextView)
+
+        var celsiusSeekBar = findViewById<SeekBar>(R.id.celsiusseekBar)
+
+
+        var fahrenheitSeekBar = findViewById<SeekBar>(R.id.fahrenheitseekbar)
+        celsiusSeekBar.max = 100
+        fahrenheitSeekBar.max = 212
+        var celsiusVal = 0.0;
+        var fahrenheitVal = 32.0;
+        celsiusSeekBar.progress = celsiusVal.toInt()
+        fahrenheitSeekBar.progress = fahrenheitVal.toInt()
+
+        celsiusSeekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    celsiusVal = progress.toDouble()
+                    fahrenheitVal = ((celsiusVal * 9) / 5) + 32
+                    updateTextViews(celsiusVal, fahrenheitVal)
+                    fahrenheitSeekBar.progress = fahrenheitVal.toInt()
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        })
+        fahrenheitSeekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    fahrenheitVal = progress.toDouble()
+                    celsiusVal =((fahrenheitVal-32)*5)/9
+                    updateTextViews(celsiusVal,fahrenheitVal)
+                    celsiusSeekBar.progress = celsiusVal.toInt()
+                }
+
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
+        updateTextViews(celsiusVal,fahrenheitVal)
+
+
+    }
+
+    private fun updateTextViews(celsiusVal: Double, fahrenheitVal: Double) {
+        celsiusTextView.text = "${celsiusVal}"
+        fahrenheitTextView.text="${fahrenheitVal}"
+    }
+
+}
